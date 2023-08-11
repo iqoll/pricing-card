6b5b199dfa1a5dabae1c2f1e9b77152dcba5ff15
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-08-11 10:20:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'serif': ['Merriweather', 'Georgia'],
+      'mono': ['Poppins', 'SFMono-Regular'],
+    }
+  },
+  plugins: [],
+}
+
